ecdc0af413e87a7fddd74aeec7f6575e9939e80a
DacNguyen99 <datnguyen2314@gmail.com>
2023-10-26 21:01:07 +0700

    Init for Ex7

--- STM32Project/Core/Src/main.c
+++ STM32Project/Core/Src/main.c
@@ -133,9 +133,9 @@ static void MX_TIM2_Init(void);
 /* USER CODE BEGIN 0 */
 int timer0_counter = 0;
 int timer0_flag = 0;
-int TIME_CYCLE = 10;
+int TIMER_CYCLE = 10;
 void setTimer0 (int duration) {
-	timer0_counter = duration / TIME_CYCLE;
+	timer0_counter = duration / TIMER_CYCLE;
 	timer0_flag = 0;
 }
 void timer_run () {
@@ -181,12 +181,47 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setTimer0(1000);
+  int hour = 15, minute = 8, second = 50;
+    void updateClockBuffer () {
+    	if (minute <= 9) {
+    		led_buffer[2] = 0;
+    		led_buffer[3] = minute;
+    	}
+    	else {
+    		led_buffer[2] = minute / 10;
+    		led_buffer[3] = minute % 10;
+    	}
+
+    	if (hour <= 9) {
+    		led_buffer[0] = 0;
+    		led_buffer[1] = hour;
+    	}
+    	else {
+    		led_buffer[0] = hour / 10;
+    		led_buffer[1] = hour % 10;
+    	}
+    }
+
+  // Idea: Use setTimer0(1000) and TIMER_CYCLE = 10, so timer0_counter = 1000 / 10 = 100 => The LED toggles every second
+  setTimer0(10);
   while (1)
   {
     if (timer0_flag == 1) {
     	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-    	setTimer0(2000);
+    	setTimer0(1000);
+        second++;
+        if (second >= 60) {
+        	second = 0;
+        	minute++;
+        }
+        if (minute >= 60) {
+        	minute = 0;
+        	hour++;
+        }
+        if (hour >= 24) {
+        	hour = 0;
+        }
+        updateClockBuffer();
     }
   }
   /* USER CODE END 3 */
